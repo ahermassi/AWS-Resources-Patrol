66b0ef86cb946c77ed1dd271954ed63ed997a33e
Anouer Hermassi <ahermass@stevens.edu>
2020-05-21 15:14:09 -0400

    Revert "Create lambda LayerVersion"
    This reverts commit bdac4a4c8f20667ffe876b8ef1dcb655f6608210.

--- utils/layer.py
+++
@@ -1,8 +0,0 @@
-import os
-from .clean import clean_layer
-
-
-def layer():
-    clean_layer()
-    os.system('pip3 install --isolated --disable-pip-version-check -Ur requirements.txt -t ./layer/')
-
